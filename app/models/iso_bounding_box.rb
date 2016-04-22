3c25288286182b8e9691c809f0535ac0f14270c5
cduma <cv.duma@gmail.com>
2016-04-22 09:02:29 -0400

    resolve CMR-2700 bounding box support for CSW GetRecords

---
+++ app/models/iso_bounding_box.rb
@@ -0,0 +1,2 @@
+class IsoBoundingBox
+end
---
+++ spec/models/ogc_filter_spatial_spec.rb
@@ -0,0 +1,194 @@
+require 'spec_helper'
+
+RSpec.describe OgcFilter do
+  describe 'OGC TempExtent_begin and TempExtent_end Filter Tests' do
+    it 'is possible to generate a TempExtent_begin AFTER CMR query from a GetRecords POST XML request' do
+      start_time_only_constraint_get_records_request_xml = <<-eos
+<csw:GetRecords maxRecords="10" outputFormat="application/xml"
+    outputSchema="http://www.isotc211.org/2005/gmd" resultType="results" service="CSW"
+    startPosition="1" version="2.0.2" xmlns="http://www.opengis.net/cat/csw/2.0.2"
+    xmlns:csw="http://www.opengis.net/cat/csw/2.0.2" xmlns:gmd="http://www.isotc211.org/2005/gmd"
+    xmlns:gml="http://www.opengis.net/gml" xmlns:ogc="http://www.opengis.net/ogc"
+    xmlns:rim="urn:oasis:names:tc:ebxml-regrep:xsd:rim:3.0">
+    <csw:Query typeNames="csw:Record">
+        <csw:ElementSetName>full</csw:ElementSetName>
+        <csw:Constraint version="1.1.0" xmlns:csw="http://www.opengis.net/cat/csw/2.0.2">
+            <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+                <ogc:And>
+                    <ogc:PropertyIsGreaterThanOrEqualTo>
+                        <ogc:PropertyName>TempExtent_begin</ogc:PropertyName>
+                        <ogc:Literal>1990-09-03T00:00:01Z</ogc:Literal>
+                    </ogc:PropertyIsGreaterThanOrEqualTo>
+                </ogc:And>
+            </ogc:Filter>
+        </csw:Constraint>
+    </csw:Query>
+</csw:GetRecords>
+      eos
+      request_body_xml = Nokogiri::XML(start_time_only_constraint_get_records_request_xml)
+      filter = request_body_xml.at_xpath("//csw:GetRecords//csw:Query//csw:Constraint//ogc:Filter",
+                                         'csw' => 'http://www.opengis.net/cat/csw/2.0.2',
+                                         'ogc' => 'http://www.opengis.net/ogc')
+      expect(filter).not_to eq nil
+      cmr_query_params = Hash.new
+      helper = OgcFilter.new(filter, cmr_query_params)
+      helper.process_temporal
+      expect(cmr_query_params['temporal[]']).to eq('1990-09-03T00:00:01Z/')
+    end
+
+    it 'is possible to generate a TempExtent_end BEFORE CMR query from a GetRecords POST XML request' do
+      start_time_only_constraint_get_records_request_xml = <<-eos
+<csw:GetRecords maxRecords="10" outputFormat="application/xml"
+    outputSchema="http://www.isotc211.org/2005/gmd" resultType="results" service="CSW"
+    startPosition="1" version="2.0.2" xmlns="http://www.opengis.net/cat/csw/2.0.2"
+    xmlns:csw="http://www.opengis.net/cat/csw/2.0.2" xmlns:gmd="http://www.isotc211.org/2005/gmd"
+    xmlns:gml="http://www.opengis.net/gml" xmlns:ogc="http://www.opengis.net/ogc"
+    xmlns:rim="urn:oasis:names:tc:ebxml-regrep:xsd:rim:3.0">
+    <csw:Query typeNames="csw:Record">
+        <csw:ElementSetName>full</csw:ElementSetName>
+        <csw:Constraint version="1.1.0" xmlns:csw="http://www.opengis.net/cat/csw/2.0.2">
+            <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+                <ogc:And>
+                    <ogc:PropertyIsLessThanOrEqualTo>
+                        <ogc:PropertyName>TempExtent_end</ogc:PropertyName>
+                        <ogc:Literal>1990-09-03T00:00:01Z</ogc:Literal>
+                    </ogc:PropertyIsLessThanOrEqualTo>
+                </ogc:And>
+            </ogc:Filter>
+        </csw:Constraint>
+    </csw:Query>
+</csw:GetRecords>
+      eos
+      request_body_xml = Nokogiri::XML(start_time_only_constraint_get_records_request_xml)
+      filter = request_body_xml.at_xpath("//csw:GetRecords//csw:Query//csw:Constraint//ogc:Filter",
+                                         'csw' => 'http://www.opengis.net/cat/csw/2.0.2',
+                                         'ogc' => 'http://www.opengis.net/ogc')
+      expect(filter).not_to eq nil
+      cmr_query_params = Hash.new
+      helper = OgcFilter.new(filter, cmr_query_params)
+      helper.process_temporal
+      expect(cmr_query_params['temporal[]']).to eq('/1990-09-03T00:00:01Z')
+    end
+
+    it 'is possible to generate a TempExtent_begin TempExtent_end range CMR query from a GetRecords POST XML request' do
+      start_time_only_constraint_get_records_request_xml = <<-eos
+<csw:GetRecords maxRecords="10" outputFormat="application/xml"
+    outputSchema="http://www.isotc211.org/2005/gmd" resultType="results" service="CSW"
+    startPosition="1" version="2.0.2" xmlns="http://www.opengis.net/cat/csw/2.0.2"
+    xmlns:csw="http://www.opengis.net/cat/csw/2.0.2" xmlns:gmd="http://www.isotc211.org/2005/gmd"
+    xmlns:gml="http://www.opengis.net/gml" xmlns:ogc="http://www.opengis.net/ogc"
+    xmlns:rim="urn:oasis:names:tc:ebxml-regrep:xsd:rim:3.0">
+    <csw:Query typeNames="csw:Record">
+        <csw:ElementSetName>full</csw:ElementSetName>
+        <csw:Constraint version="1.1.0" xmlns:csw="http://www.opengis.net/cat/csw/2.0.2">
+            <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+                <ogc:And>
+                    <ogc:PropertyIsGreaterThanOrEqualTo>
+                        <ogc:PropertyName>TempExtent_begin</ogc:PropertyName>
+                        <ogc:Literal>1990-09-03T00:00:01Z</ogc:Literal>
+                    </ogc:PropertyIsGreaterThanOrEqualTo>
+                    <ogc:PropertyIsLessThanOrEqualTo>
+                        <ogc:PropertyName>TempExtent_end</ogc:PropertyName>
+                        <ogc:Literal>2016-09-06T23:59:59Z</ogc:Literal>
+                    </ogc:PropertyIsLessThanOrEqualTo>
+                </ogc:And>
+            </ogc:Filter>
+        </csw:Constraint>
+    </csw:Query>
+</csw:GetRecords>
+      eos
+      request_body_xml = Nokogiri::XML(start_time_only_constraint_get_records_request_xml)
+      filter = request_body_xml.at_xpath("//csw:GetRecords//csw:Query//csw:Constraint//ogc:Filter",
+                                         'csw' => 'http://www.opengis.net/cat/csw/2.0.2',
+                                         'ogc' => 'http://www.opengis.net/ogc')
+      expect(filter).not_to eq nil
+      cmr_query_params = Hash.new
+      helper = OgcFilter.new(filter, cmr_query_params)
+      helper.process_temporal
+      expect(cmr_query_params['temporal[]']).to eq('1990-09-03T00:00:01Z/2016-09-06T23:59:59Z')
+    end
+
+    it 'is NOT possible to generate a CMR query from a GetRecords POST XML request with an invalid TempExtent_begin date' do
+      start_time_only_constraint_get_records_request_xml = <<-eos
+<csw:GetRecords maxRecords="10" outputFormat="application/xml"
+    outputSchema="http://www.isotc211.org/2005/gmd" resultType="results" service="CSW"
+    startPosition="1" version="2.0.2" xmlns="http://www.opengis.net/cat/csw/2.0.2"
+    xmlns:csw="http://www.opengis.net/cat/csw/2.0.2" xmlns:gmd="http://www.isotc211.org/2005/gmd"
+    xmlns:gml="http://www.opengis.net/gml" xmlns:ogc="http://www.opengis.net/ogc"
+    xmlns:rim="urn:oasis:names:tc:ebxml-regrep:xsd:rim:3.0">
+    <csw:Query typeNames="csw:Record">
+        <csw:ElementSetName>full</csw:ElementSetName>
+        <csw:Constraint version="1.1.0" xmlns:csw="http://www.opengis.net/cat/csw/2.0.2">
+            <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+                <ogc:And>
+                    <ogc:PropertyIsGreaterThanOrEqualTo>
+                        <ogc:PropertyName>TempExtent_begin</ogc:PropertyName>
+                        <ogc:Literal>INVALID_DATE_HERE</ogc:Literal>
+                    </ogc:PropertyIsGreaterThanOrEqualTo>
+                </ogc:And>
+            </ogc:Filter>
+        </csw:Constraint>
+    </csw:Query>
+</csw:GetRecords>
+      eos
+      request_body_xml = Nokogiri::XML(start_time_only_constraint_get_records_request_xml)
+      filter = request_body_xml.at_xpath("//csw:GetRecords//csw:Query//csw:Constraint//ogc:Filter",
+                                         'csw' => 'http://www.opengis.net/cat/csw/2.0.2',
+                                         'ogc' => 'http://www.opengis.net/ogc')
+      expect(filter).not_to eq nil
+      cmr_query_params = Hash.new
+      helper = OgcFilter.new(filter, cmr_query_params)
+      #expect(helper.process_temporal).to raise_exception(OwsException, "message HERE")
+      begin
+        helper.process_temporal
+      rescue OwsException => e
+        expect(e.exception_text).to eq('OgcFilter.validate_iso_date INVALID_DATE_HERE is NOT in the required ISO8601 format yyyy-MM-ddTHH:mm:ssZ')
+        expect(e.http_code).to eq('400')
+        expect(e.exception_code).to eq('InvalidParameterValue')
+        expect(e.locator).to eq('INVALID_DATE_HERE')
+      end
+    end
+
+    it 'is NOT possible to generate a CMR query from a GetRecords POST XML request with an invalid TempExtent_end date' do
+      start_time_only_constraint_get_records_request_xml = <<-eos
+<csw:GetRecords maxRecords="10" outputFormat="application/xml"
+    outputSchema="http://www.isotc211.org/2005/gmd" resultType="results" service="CSW"
+    startPosition="1" version="2.0.2" xmlns="http://www.opengis.net/cat/csw/2.0.2"
+    xmlns:csw="http://www.opengis.net/cat/csw/2.0.2" xmlns:gmd="http://www.isotc211.org/2005/gmd"
+    xmlns:gml="http://www.opengis.net/gml" xmlns:ogc="http://www.opengis.net/ogc"
+    xmlns:rim="urn:oasis:names:tc:ebxml-regrep:xsd:rim:3.0">
+    <csw:Query typeNames="csw:Record">
+        <csw:ElementSetName>full</csw:ElementSetName>
+        <csw:Constraint version="1.1.0" xmlns:csw="http://www.opengis.net/cat/csw/2.0.2">
+            <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+                <ogc:And>
+                    <ogc:PropertyIsLessThanOrEqualTo>
+                        <ogc:PropertyName>TempExtent_end</ogc:PropertyName>
+                        <ogc:Literal>INVALID_DATE_HERE</ogc:Literal>
+                    </ogc:PropertyIsLessThanOrEqualTo>
+                </ogc:And>
+            </ogc:Filter>
+        </csw:Constraint>
+    </csw:Query>
+</csw:GetRecords>
+      eos
+      request_body_xml = Nokogiri::XML(start_time_only_constraint_get_records_request_xml)
+      filter = request_body_xml.at_xpath("//csw:GetRecords//csw:Query//csw:Constraint//ogc:Filter",
+                                         'csw' => 'http://www.opengis.net/cat/csw/2.0.2',
+                                         'ogc' => 'http://www.opengis.net/ogc')
+      expect(filter).not_to eq nil
+      cmr_query_params = Hash.new
+      helper = OgcFilter.new(filter, cmr_query_params)
+      #expect(helper.process_temporal).to raise_exception(OwsException, "message HERE")
+      begin
+        helper.process_temporal
+      rescue OwsException => e
+        expect(e.exception_text).to eq('OgcFilter.validate_iso_date INVALID_DATE_HERE is NOT in the required ISO8601 format yyyy-MM-ddTHH:mm:ssZ')
+        expect(e.http_code).to eq('400')
+        expect(e.exception_code).to eq('InvalidParameterValue')
+        expect(e.locator).to eq('INVALID_DATE_HERE')
+      end
+    end
+
+  end
+end
