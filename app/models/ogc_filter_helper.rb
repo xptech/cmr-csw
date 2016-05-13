6668aec7bad92a02408dd4977803400b16b08e37
cduma <cv.duma@gmail.com>
2016-05-13 10:21:00 -0400

    refactor - added specs and more robust error handling for BBOX

--- app/models/ogc_filter_helper.rb
+++ app/models/ogc_filter_helper.rb
@@ -6,12 +6,12 @@ class OgcFilterHelper
     property_name_node_set = ogc_filter.xpath(xpath_string, 'ogc' => 'http://www.opengis.net/ogc')
     if (property_name_node_set != nil && property_name_node_set[0] != nil)
       literal_node = property_name_node_set[0].next_element
-      if (literal_node != nil)
+      if (literal_node != nil && literal_node.name == 'Literal')
         literal_value = literal_node.text
         cmr_query_hash["#{cmr_parameter_name}"] = literal_value
-      end
-      if(wildcard_flag)
-        process_wildcard(cmr_query_hash, literal_node, cmr_parameter_name)
+        if (wildcard_flag)
+          process_wildcard(cmr_query_hash, literal_node, cmr_parameter_name)
+        end
       end
     end
     cmr_query_hash
