743956a71dba33e3c0aa22a67e6912169d5e4ed3
dnewman <douglas.j.newman@nasa.gov>
2016-06-02 14:12:13 -0400

    CMR-2859 - removed sit reference

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,5 +38,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.cmr_search_endpoint = 'https://cmr.sit.earthdata.nasa.gov/search'
 end
